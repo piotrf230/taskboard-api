3e7505b5d954af4a1ac53651d627ccccda7ec31b
piotrf230 <piotrf230@gmail.com>
2023-04-26 20:06:08 +0200

    auth works

--- TaskboardProject/api/v1/user/urls.py
+++ TaskboardProject/api/v1/user/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
-
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify-token/', TokenVerifyView.as_view(), name='token_verify'),
 ]
